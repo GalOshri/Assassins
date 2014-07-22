bb54d1b87061cb435911fa3c9ba5835ba0d6042f
GalOshri <galoshri@gmail.com>
2014-07-21 22:04:12 -0700

    Initial Commit

---
+++ Assassins/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Assassins
+//
+//  Created by Gal Oshri on 7/21/14.
+//  Copyright (c) 2014 Kefi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
