fa792d4af1397c5758ef90f5999951d46f4206b5
GalOshri <galoshri@gmail.com>
2014-07-27 10:26:03 -0700

    Basics, but not working

---
+++ iOS/Assassins/SnipeSubmitView.h
@@ -0,0 +1,15 @@
+//
+//  SnipeSubmitView.h
+//  Assassins
+//
+//  Created by Gal Oshri on 7/24/14.
+//  Copyright (c) 2014 Kefi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SnipeSubmitView : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIImageView *snipeImageView;
+
+@end
--- iOS/Assassins/ViewController.m
+++ iOS/Assassins/ViewController.m
@@ -7,23 +7,78 @@
 //
 
 #import "ViewController.h"
+#import "SnipeSubmitView.h"
 
 @interface ViewController ()
+
 
 @end
 
 @implementation ViewController
 
+UIImagePickerController *picker;
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"SnipeSegue"]) {
+        if ([segue.destinationViewController isKindOfClass:[SnipeSubmitView class]])
+        {
+            SnipeSubmitView *ssv = (SnipeSubmitView *)segue.destinationViewController;
+            ssv.snipeImageView.image = (UIImage *)sender;
+        }
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    picker  = [[UIImagePickerController alloc] init];
+    
+    [picker setDelegate:self];
+    [picker setSourceType:UIImagePickerControllerSourceTypeCamera];
+    [picker setMediaTypes:@[@"public.image"]]; //specify image (not video)
+    [picker setShowsCameraControls:NO]; //hide default camera controls
+    [picker setNavigationBarHidden:YES];
+    [picker setToolbarHidden:YES];
+    [picker setAllowsEditing:NO];
+    picker.view.frame = CGRectMake(0, 0, picker.view.frame.size.width, picker.view.frame.size.height * 0.4);
+    [self.view addSubview:picker.view];
+ //   self.imageView.frame = CGRectMake(self.imageView.frame.origin.x, self.imageView.frame.origin.y, picker.view.frame.size.width, picker.view.frame.size.height);
+    
+    
+    
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)takePicture:(UIButton *)sender {
+    
+    [picker takePicture];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *chosenImage = info[UIImagePickerControllerOriginalImage];
+  //  self.imageView.image = chosenImage;
+    
+    [self performSegueWithIdentifier:@"SnipeSegue" sender:chosenImage];
+    
+    
+    //  [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
 }
 
 @end
