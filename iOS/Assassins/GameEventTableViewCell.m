f13a71966c6443834287b2842f418dd0bb9c00ef
p-stav <paulstavropoulos@gmail.com>
2014-08-04 01:25:10 -0700

    + controllers for elements and setting things up
    hi

---
+++ iOS/Assassins/GameEventTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  GameEventTableViewCell.m
+//  Assassins
+//
+//  Created by Paul Stavropoulos on 8/4/14.
+//  Copyright (c) 2014 Kefi. All rights reserved.
+//
+
+#import "GameEventTableViewCell.h"
+
+@implementation GameEventTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ iOS/Assassins/GameTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameTableViewController.h
+//  Assassins
+//
+//  Created by Paul Stavropoulos on 8/4/14.
+//  Copyright (c) 2014 Kefi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameTableViewController : UITableViewController
+
+@end
