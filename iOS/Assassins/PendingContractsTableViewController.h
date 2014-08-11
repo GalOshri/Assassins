9d5e4c4f9587ae4018344bb562026f37bbbf0bd0
p-stav <paulstavropoulos@gmail.com>
2014-08-10 22:44:15 -0700

    created class
    hi

---
+++ iOS/Assassins/PendingContractsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PendingContractsTableViewController.h
+//  Assassins
+//
+//  Created by Paul Stavropoulos on 8/10/14.
+//  Copyright (c) 2014 Kefi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PendingContractsTableViewController : UITableViewController
+
+@end
