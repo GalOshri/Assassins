5b77daa706e29b13339cba38e6383e43d4574c1f
p-stav <paulstavropoulos@gmail.com>
2014-08-10 22:25:39 -0700

    negligible changes
    hi

--- iOS/Assassins/SnipeSubmitView.m
+++ iOS/Assassins/SnipeSubmitView.m
@@ -26,7 +26,6 @@
 
 /*
  #pragma mark - Navigation
- 
  // In a storyboard-based application, you will often want to do a little preparation before navigation
  - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
  {
