c682374ad9a5655e13b232b10bdc3d8037302464
p-stav <paulstavropoulos@gmail.com>
2014-11-10 14:38:30 -0800

    Revert d3cfe49..df1e465
    This rolls back to commit d3cfe49f65b8db030061f94aedd8ca33772ef2c2.

--- iOS/Assassins/AssassinsService.m
+++ iOS/Assassins/AssassinsService.m
@@ -47,6 +47,7 @@
             // NOPENDING --------------------------------------------*************************************
             NSDictionary *completedContractDict = [[NSDictionary alloc] initWithObjectsAndKeys:contractObject.objectId, @"contractId", nil];
             NSString *responseString = [PFCloud callFunction:@"completedContract" withParameters:completedContractDict];
+            NSLog(@"%@", responseString);
         }];
     }
 }
@@ -395,6 +396,7 @@
     // call cloud code to add username to array of invalidateVoters, check if it has been overturned, and clean function if not
     NSDictionary *declineSnipeDict = [[NSDictionary alloc] initWithObjectsAndKeys:gameId, @"gameId", contractId, @"contractId", [PFUser currentUser].objectId, @"userId", nil];
     NSString *responseString = [PFCloud callFunction:@"checkInvalidatedSnipe" withParameters:declineSnipeDict];
+    NSLog(@"%@", responseString);
 }
 
 
@@ -402,6 +404,7 @@
 {
     NSDictionary *pendingContractDict = [[NSDictionary alloc] initWithObjectsAndKeys:game.gameId, @"gameId", contract.contractId, @"contractId",contract.targetName, @"targetName", nil];
     NSString *responseString = [PFCloud callFunction:@"startPendingContractProcess" withParameters:pendingContractDict];
+    NSLog(@"%@", responseString);
 }
 
 
