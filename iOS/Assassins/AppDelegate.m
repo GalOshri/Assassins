6e027789c953583a8a5b42d011d50c9a8d0e1b3c
GalOshri <galoshri@gmail.com>
2014-11-23 12:03:15 -0800

    push permission

--- iOS/Assassins/AppDelegate.m
+++ iOS/Assassins/AppDelegate.m
@@ -29,22 +29,7 @@
     [FBProfilePictureView class];
     [FBFriendPickerViewController class];
     
-    // Register for push notifications
-    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
-        UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
-                                                        UIUserNotificationTypeBadge |
-                                                        UIUserNotificationTypeSound);
-        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
-                                                                                 categories:nil];
-        [application registerUserNotificationSettings:settings];
-        [application registerForRemoteNotifications];
-    }
-    else {
-        // Register for Push Notifications before iOS 8
-        [application registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
-                                                         UIRemoteNotificationTypeAlert |
-                                                         UIRemoteNotificationTypeSound)];
-    }
+    
 
     // Deal with push notification
     if (launchOptions != nil)
